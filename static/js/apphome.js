d5045e95f63b9e1089c1f1d14f80bb3b99c073b4
EC2 Default User <ec2-user@ip-172-31-51-204.ec2.internal>
2024-06-16 01:15:56 +0000

    imagenes

---
+++ static/js/apphome.js
@@ -0,0 +1,11 @@
+function registrar_mascota(){
+    window.location = "/registrar"
+}
+
+function consultar_mascota(){
+    window.location = "/consultar"
+}
+
+function consulta_urgencias(){
+    window.location = "/urgencias"
+}
